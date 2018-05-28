af55bb45d7cfc1be10f9216f0300cec499340c70
Takahiro Hida <taka.hida.0123@gmail.com>
2018-05-28 16:13:07 +0900

    課題

--- rb.rb
+++ rb.rb
@@ -1,7 +1,26 @@
-p "Branding enjineer"
-p "Hello"
-p "HR Tech"
+p "Q1"
+puts "初めてのRuby"
 
-puts "1"
-puts "2"
-puts "3"
+p "Q2"
+puts "RUby"+"始めました"
+
+p "Q3"
+puts 3+3
+puts 3-3
+puts 3*3
+puts 3/3
+
+p "Q4&Q5"
+num_array = [1,2,3,4]
+
+p "Q6"
+p num_array[0]
+p num_array[1]
+p num_array[2]
+p num_array[3]
+
+p "Q7&Q8"
+Me = {name:'Takahiro' , bloodtype:'A' , birthdate:"1996/1/23"}
+puts Me[:name]
+puts Me[:bloodtype]
+puts Me[:birthdate]
